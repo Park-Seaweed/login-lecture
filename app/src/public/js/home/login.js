e2816fad60aa9ef140abe0fe47526cfe30c1fb7a
박민혁 <bagminhyeog@bagminhyeog-ui-MacBookPro.local>
2022-05-25 22:11:05 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,8 +17,16 @@ function login(){
             "Content-Type": "application/json"
         },
         body : JSON.stringify(req),
-    }).then((res) => res.json())
+    })
+    .then((res) => res.json())
     .then((res) => {
-        
+        if (res.sucess) {
+            location.href = "/"; 
+        }else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
     });
 }
